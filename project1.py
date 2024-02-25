957cd98c61f16fab9ca622a0cd515b9bee34b1bd
Stina <stina.ida.fredriksson@gmail.com>
2024-02-25 15:29:44 +0100

    Added analytical solution

--- project1.py
+++ project1.py
@@ -6,9 +6,25 @@ import math
 import numpy as np
 
 
-def potential(U0,r,rmax):
+def potential(U0, r, rmax):
     if r <= rmax:
         return U0
     else:
         return 0
+
+
+def theta_b_less(b, rmax, U0, E):
+    return 2*math.asin(b/rmax/(1-U0/E)) - 2*math.asin(b/rmax)    
+
+def theta_b_greater(b, rmax):
+    return math.pi - 2*math.asin(b/rmax)
+
+
+def analytical(E, U0):
+
+    if E < U0:
+        return theta_b_less
+    else:
+        return theta_b_greater
+
     
