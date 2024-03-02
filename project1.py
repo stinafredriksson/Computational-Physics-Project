6208d2ea9670ed536261fb97fa597a66a8c83925
Stina <stina.ida.fredriksson@gmail.com>
2024-03-02 15:10:46 +0100

    saving progress

--- project1.py
+++ project1.py
@@ -25,40 +25,45 @@ def theta_b_less(b, rmax):
 def theta_b_greater(b, rmax, U0, E):
     return 2*math.asin(b/rmax/(1-U0/E)**0.5) - 2*math.asin(b/rmax) 
     
-def f_E_less(p):       
-    return 1/(p**2+b)**2*(1-b**2/(p**2+b)**2)**-0.5*2*p
+def f_E_less(p):
+    if p ==0:
+        return 0
+    else:       
+        return 1/(p**2+b)**2*(1-b**2/(p**2+b)**2)**-0.5*2*p
 
 def f_E_greater_1(p):
     return 1/(p**2+rmin)**2*(1-b**2/(p**2+rmin)**2)**-0.5*2*p
 
 def f_E_greater_2(p):
-    # p = (r-rmin)**0.5
-    return 1/(p**2+rmin)**2*(1-b**2/(p**2+rmin)**2 - U0/E)**-0.5*2*p
+    if p ==0:
+        return 0
+    else:
+        return 1/(p**2+rmin)**2*(1-b**2/(p**2+rmin)**2 - U0/E)**-0.5*2*p
 
 
 ## GLOBAL VARIABLES ##
 
-U0 = 1
-E = 2
+U0 = 2
+E = 1
 rmax = 5
-b = 2
+b = 0.5
 # b = [rmax/14*i for i in range(15)]
 rmin = r_min(b, U0, E, rmax)
 # b = [rmax/14*i for i in range(15)] # between 0 and rmax
 
 def main():
     N = 20
-    # print(rmin)
     if E < U0:
-        # print('hello')
         theta_num = 2*b*boole(f_E_less, 0.00001, (rmax-b)**0.5, N)
         theta_analytic_values = theta_b_less(b, rmax)
     else:
-        theta_num = 2*b*(boole(f_E_greater_1, (b-rmin)**0.5, (rmax-rmin)**0.5, N) - boole(f_E_greater_2, 0, (rmax-rmin)**0.5, N))
+        theta_num = 2*b* boole(f_E_greater_2, 0, (rmax-rmin)**0.5, N)
         theta_analytic_values = theta_b_greater(b, rmax, U0, E)
     
     print(f"Numerical solution: {theta_num}")
     print(f"Analytical solution: {theta_analytic_values}")
+    print(rmin)
+
 
 if __name__ == '__main__':
     main()
