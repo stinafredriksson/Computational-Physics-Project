4ab7838f6dd9137edff06d53592faac00dc856b8
Marmingen <maja.marminge@gmail.com>
2024-03-03 15:57:06 +0100

    added plot for analytical theta

--- project1.py
+++ project1.py
@@ -5,6 +5,7 @@
 import math
 import numpy as np
 from functions import boole
+import matplotlib.pyplot as plt
 
 #################################################################
 ## FUNCTIONS
@@ -25,8 +26,16 @@ def theta_b_less(b, rmax):
     return math.pi - 2*math.asin(b/rmax)
 
 def theta_b_greater(b, rmax, U0, E):
-    return 2*math.asin(b/rmax/(1-U0/E)**0.5) - 2*math.asin(b/rmax) 
-    
+    if 0 <= b < rmax*math.sqrt(1-U0/E):
+        return 2*math.asin(b/rmax/(1-U0/E)**0.5) - 2*math.asin(b/rmax) 
+    elif b<rmax:
+        return theta_b_less(b,rmax)
+    elif b>=rmax:
+        return 0
+    else:
+        raise ValueError("b cannot be below zero")
+
+
 def f_E_less(p):
     if p ==0:
         return 0
@@ -41,12 +50,30 @@ def f_E_greater_2(p):
         return 0
     else:
         return 1/(p**2+rmin)**2*(1-b**2/(p**2+rmin)**2 - U0/E)**-0.5*2*p
+    
+
+def plot_greater():
+
+    bs = np.linspace(0,rmax,200)
+
+
+    plt.title("Deflection angle over impact parameter")
+    plt.plot(bs,[theta_b_less(bi,rmax) for bi in bs],c="C0",linestyle="--",label=r"$E\leq V$")
+    plt.plot(bs, [theta_b_greater(bi,rmax, U0, E) for bi in bs],c="C1",linestyle="--",label=r"E>V")
+    plt.grid(linestyle="--")
+    plt.xlabel("b")
+    plt.xticks([0,rmax*math.sqrt(1-U0/E),rmax],["0",r"$r_\text{max}\sqrt{1-\frac{U_0}{E}}$",r"$r_\text{max}$"])
+    plt.ylabel(r"$\Theta(b)$")
+    plt.legend()
+    plt.yticks([0,math.pi/2,math.pi],[0,r"$\pi/2$",r"$\pi$"])
+    plt.subplots_adjust(bottom=0.145)
+    plt.show()
 
 #################################################################
 ## GLOBALS
 
 U0 = 2
-E = 1
+E = 4
 rmax = 5
 b = 0.5
 # b = [rmax/14*i for i in range(15)]
@@ -57,17 +84,20 @@ rmin = r_min(b, U0, E, rmax)
 ## MAIN
 
 def main():
-    N = 20
-    if E < U0:
-        theta_num = 2*b*boole(f_E_less, 0.00001, (rmax-b)**0.5, N)
-        theta_analytic_values = theta_b_less(b, rmax)
-    else:
-        theta_num = 2*b* boole(f_E_greater_2, 0, (rmax-rmin)**0.5, N)
-        theta_analytic_values = theta_b_greater(b, rmax, U0, E)
+
+    plot_greater()
+
+    # N = 20
+    # if E < U0:
+    #     theta_num = 2*b*boole(f_E_less, 0.00001, (rmax-b)**0.5, N)
+    #     theta_analytic_values = theta_b_less(b, rmax)
+    # else:
+    #     theta_num = 2*b* boole(f_E_greater_2, 0, (rmax-rmin)**0.5, N)
+    #     theta_analytic_values = theta_b_greater(b, rmax, U0, E)
     
-    print(f"Numerical solution: {theta_num}")
-    print(f"Analytical solution: {theta_analytic_values}")
-    print(rmin)
+    # print(f"Numerical solution: {theta_num}")
+    # print(f"Analytical solution: {theta_analytic_values}")
+    # print(rmin)
 
 
 #################################################################
