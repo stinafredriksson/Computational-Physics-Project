7dde3234b75679f0b75d9091e0b5c568a7ffbd18
Marmingen <maja.marminge@gmail.com>
2024-03-03 16:52:42 +0100

    expanded project 2

--- project2.py
+++ project2.py
@@ -4,11 +4,15 @@
 ## IMPORTS
 
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 #################################################################
 ## FUNCTIONS
 
-rmax = 1
+a = 3
+rmax = 3*a
+V0 = 1
 
 def abs_db_dtheta(b):
     return rmax/2*math.sqrt(1-b**2/rmax**2)
@@ -16,3 +20,21 @@ def abs_db_dtheta(b):
 def cross_section(b,theta):
     return b/math.sin(theta)*abs_db_dtheta(b)
 
+def potential(r):
+    if r <= rmax:
+        return V0*((a/r)**12-(a/r)**6)
+
+def plot_potential():
+    
+    rs = np.linspace(0,rmax,200)
+
+    plt.plot(rs,[potential(r) for r in rs])
+    plt.show()
+    
+
+def main():
+    plot_potential()
+
+
+if __name__ == "__main__":
+    main()
