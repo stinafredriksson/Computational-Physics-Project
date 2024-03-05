4db21e199fafb3bca9f85aa68d616aafe6d2e8e9
Marmingen <maja.marminge@gmail.com>
2024-03-05 12:14:11 +0100

    lil' update

--- project2.py
+++ project2.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 #################################################################
 ## FUNCTIONS
 
-a = 3
+a = 5
 rmax = 3*a
 V0 = 1
 
@@ -22,13 +22,24 @@ def cross_section(b,theta):
 
 def potential(r):
     if r <= rmax:
-        return V0*((a/r)**12-(a/r)**6)
+        return 4*V0*((a/r)**12-(a/r)**6)
 
 def plot_potential():
     
-    rs = np.linspace(0,rmax,200)
+    rs1 = np.linspace(0.2*a,a,200)
+    rs2 = np.linspace(a,3*a,200)
 
-    plt.plot(rs,[potential(r) for r in rs])
+    plt.plot(rs1,[potential(r) for r in rs1], c="C0",label=r"Positive")
+    plt.plot(rs2,[potential(r) for r in rs2], c="C1",label=r"Negative")
+    # plt.plot(rs,[4*V0*(a/r)**12 for r in rs], c="C1",linestyle="dotted",label=r"$4V_0\left(\frac{a}{r}\right)^{12}$")
+    # plt.plot(rs,[a**6/(a**6-r**6) for r in rs])
+    plt.legend()
+    plt.grid(linestyle="--")
+    plt.xlabel("r")
+    plt.ylabel("V")
+    plt.title("Lennard Jones potential")
+    plt.yticks(color = "w")
+    plt.xticks([0,a,3*a], [0,r"$a$",r"$r_\text{max}$"])
     plt.show()
     
 
