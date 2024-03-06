9e84158cb63c300d1796aa2c1cd9277f9687d337
Marmingen <maja.marminge@gmail.com>
2024-03-06 01:15:40 +0100

    fixed notation

--- project1.py
+++ project1.py
@@ -3,19 +3,15 @@
 #################################################################
 
 import math
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 #################################################################
 ## FUNCTIONS
-
-def potential(U0, r, rmax):
-    if r <= rmax:
-        return U0
-    else:
-        return 0   
     
-def r_min(b, U0, E, rmax):
+def r_min(b: float, U0: float, E: float, rmax: float) -> float:
+    """calculates rmin"""
     if E > U0:
         return b/math.sqrt(1-U0/E)
     else:
@@ -39,22 +35,31 @@ def boole(f, a1: float, a2:float, N:int, b, rmin) -> float:
                    14*sum([f(a1+k*h,b,rmin) for k in range(4,N,4)]))
 
 
-def theta_b_less(b, rmax):
+def theta_b_less(b: float, rmax: float, U0: float, E: float) -> float:
+    """analytical solution for E<=U0"""
+    if U0 < 0:
+        return theta_b_greater(b,rmax,U0,E)
     return math.pi - 2*math.asin(b/rmax)
 
 
-def theta_b_greater(b, rmax, U0, E):
+def theta_b_greater(b: float, rmax: float, U0: float, E: float) -> float:
+    """analytical solution for E>U0"""
+
+    # particle enters the potential
     if 0 <= b < rmax*math.sqrt(1-U0/E):
-        return 2*math.asin(b/rmax/(1-U0/E)**0.5) - 2*math.asin(b/rmax) 
+        return 2*math.asin(b/rmax/(1-U0/E)**0.5) - 2*math.asin(b/rmax)
+    # particle reflects from the potential
     elif b<rmax:
-        return theta_b_less(b,rmax)
+        return theta_b_less(b,rmax,U0,E)
+    # particle misses the potential
     elif b>=rmax:
         return 0
     else:
         raise ValueError("b cannot be below zero")
 
 
-def integral_1(p,b,rmin):
+def integral_1(p: float, b: float, rmin: float) -> float:
+    """numerical setup for the first integral"""
     if p==0:
         if b == 0:
             return 0
@@ -63,17 +68,18 @@ def integral_1(p,b,rmin):
         return 1/(p**2+b)**2*(1-b**2/(p**2+b)**2)**-0.5*2*p
     
 
-def integral_2(p,b,rmin):
+def integral_2(p: float, b: float, rmin: float) -> float:
+    """numerical setup for the second integral"""
     if p==0:
         if b == 0:
-            return 0 
+            return 0
         return 2**0.5/b**1.5*(1-U0/E)**0.25
     else:
         return 1/(p**2+rmin)**2/(1-b**2/(p**2+rmin)**2-U0/E)**0.5*2*p
     
 
-def integrate(b, N):
-
+def integrate(b: float, N: float) -> float:
+    """integrates the two integrals using Boole integration"""
     if E>U0:
         rmin = r_min(b, U0, E, rmax)
     else:
@@ -89,24 +95,32 @@ def integrate(b, N):
     return theta_num_1-np.real(theta_num_2)
 
 
-def plot_greater():
+def plot_analytical(U0: float, E:float, rmax:float) -> None:
+
+    if 0 < E < U0:
+        raise ValueError("Cannot use this function for $E<U_0$") 
 
     bs = np.linspace(0,rmax,200)
 
     plt.title("Deflection angle over impact parameter")
-    plt.plot(bs,[theta_b_less(bi,rmax) for bi in bs],c="C0",linestyle="--",label=r"$E\leq U_0$")
-    plt.plot(bs, [theta_b_greater(bi,rmax, U0, E) for bi in bs],c="C1",linestyle="--",label=r"$E>U_0$")
+    plt.plot(bs,[theta_b_less(bi,rmax,U0,E) for bi in bs],c="C0",linestyle="--",label=r"$E\leq U_0$")
+    plt.plot(bs,[theta_b_greater(bi,rmax,U0,E) for bi in bs],c="C1",linestyle="--",label=r"$E>U_0$")
     plt.grid(linestyle="--")
     plt.xlabel("b")
-    plt.xticks([0,rmax*math.sqrt(1-U0/E),rmax],["0",r"$r_\text{max}\sqrt{1-\frac{U_0}{E}}$",r"$r_\text{max}$"])
+
+    if U0 > 0:
+        plt.xticks([0,rmax*math.sqrt(1-U0/E),rmax],["0",r"$r_\text{max}\sqrt{1-\frac{U_0}{E}}$",r"$r_\text{max}$"])
+        plt.yticks([0,math.pi/2,math.pi],[0,r"$\pi/2$",r"$\pi$"])
+    else:
+        plt.xticks([0,rmax/2,rmax],[0,r"$r_\text{max}/2$",r"$r_\text{max}$"])
+
     plt.ylabel(r"$\Theta(b)$")
     plt.legend()
-    plt.yticks([0,math.pi/2,math.pi],[0,r"$\pi/2$",r"$\pi$"])
     plt.subplots_adjust(bottom=0.145)
     plt.show()
 
 
-def plot_square():
+def plot_square() -> None:
 
     def __square(r):
         if r<=rmax:
@@ -126,29 +140,36 @@ def plot_square():
     plt.show()
 
 
-def plot_difference():
+def plot_difference() -> None:
 
     bs = np.linspace(0,rmax,2000)
 
-    N = 160
+    N = 160*4
 
     numerical = []
     theory = []
     error = []
     imag = []
 
+    times = [time.time(),time.time()] # setup for time-left estimation
+
     for i, bnp in enumerate(bs):
 
-        bi = bnp.item()
+        bi = bnp.item() # needed conversion numpy.float64 -> native float
 
-        print(f"{round(i/len(bs)*100)}",end="\r")
+        ## error bar
+        ####
+        times.pop(0)
+        times.append(time.time())
+        print(f"\r[{'#'*round(i/(len(bs)-1)*20):.<20}] {round(i/(len(bs)-1)*100):02}% |{round((times[1]-times[0])*(len(bs)-1-i)):02}s|",end="\r")
+        ####
 
         theta_num = integrate(bi, N)
 
         if E>abs(U0) or U0<0:
             theta_analytic_values=theta_b_greater(bi,rmax,U0,E)
         else:
-            theta_analytic_values=theta_b_less(bi,rmax)
+            theta_analytic_values=theta_b_less(bi,rmax,U0,E)
 
         error.append(abs(theta_num-theta_analytic_values))
         numerical.append(theta_num)
@@ -158,7 +179,6 @@ def plot_difference():
     plt.plot(bs,numerical,color="C0",linestyle="--",label="Numerical")
     plt.plot(bs,theory,color="C1",linestyle="dotted",label="Analytical")
     plt.xlabel("b")
-    plt.ylabel(r"$\Theta(b)$")
 
     if E>abs(U0) and U0>0:
         title = r"$E>|U_0|$"
@@ -205,7 +225,7 @@ def plot_difference():
     plt.show()
 
 
-def plot_ordo():
+def plot_ordo() -> None:
 
     b = 8
 
@@ -224,7 +244,7 @@ def plot_ordo():
         if E>U0:
             theta_analytic_values=theta_b_greater(b,rmax,U0,E)
         else:
-            theta_analytic_values=theta_b_less(b,rmax)
+            theta_analytic_values=theta_b_less(b,rmax,U0,E)
 
         error.append(abs(theta_num-theta_analytic_values))
 
@@ -233,10 +253,8 @@ def plot_ordo():
     
     print(f"q(N<80) = {round(np.mean(conv[:19]),2)}")
 
-
     # plt.plot(Ns[1:],conv)
     # plt.ylabel("order of convergence q")
-
             
     plt.plot(Ns,error)
     plt.xlabel("N")
@@ -244,29 +262,23 @@ def plot_ordo():
     plt.ylabel("log(|Error|)")
     plt.grid(linestyle="--")
     plt.show()
-        
 
 #################################################################
 ## GLOBALS
 
-U0 = -1
+U0 = 1
 E = 2
 rmax = 10
-# b = 0.5
-# b = [rmax/14*i for i in range(15)]
-# rmin = r_min(b, U0, E, rmax)
-# b = [rmax/14*i for i in range(15)] # between 0 and rmax
 
 #################################################################
 ## MAIN
 
 def main():
 
-    # plot_greater()
+    plot_analytical(U0,E,rmax)
     # plot_square()
-    plot_difference()
+    # plot_difference()
     # plot_ordo()
-
 
     # b = 0
     # N = 40
